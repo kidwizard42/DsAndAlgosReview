3db4a63cbcd9225beabca43eb5d4bd57fbbccc73
Darwood Antoine <darwooda@gmail.com>
2023-06-30 16:12:09 -0400

    feat:inital file strucuture and array type made

---
+++ src/app/DsAndAlgos/array/array.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-array',
+  templateUrl: './array.component.html',
+  styleUrls: ['./array.component.scss'],
+})
+export class ArrayComponent {}
