35bd15cf8610ad5f8014a344a83ecb127564b422
Darwood Antoine <darwooda@gmail.com>
2023-11-01 19:52:10 -0400

    feat: lc 100 binary tree

---
+++ src/app/leetcode/leetcode100.js
@@ -0,0 +1,14 @@
+// link:  https://leetcode.com/problems/same-tree/description/
+
+// C# solution
+// public bool IsSameTree(TreeNode p, TreeNode q) {
+
+//     if(p == q ) return true;
+
+//     if(p== null || q == null) return false;
+
+//     if(p.val != q.val) return false;
+
+//     return ( IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
+
+// }
